b99295ffe35c0717a235fd7512101a6f8103f13c
Wade Womersley <wade85@gmail.com>
2015-02-20 19:06:01 +0000

    Template interfaces

---
+++ src/Text/Interfaces/TemplatedInterface.php
@@ -0,0 +1,44 @@
+<?php
+namespace com\xcitestudios\Generic\Text\Interfaces;
+
+use stdClass;
+
+/**
+ * General interface for something that can render a template style string.
+ */
+interface TemplatedInterface
+{
+	/**
+	 * Sets the template version of the content to be used during rendering.
+	 *
+	 * @return void
+	 */
+	public function setContent($content);
+	
+	/**
+	 * Gets the template version of the content being used during rendering.
+	 *
+	 * @return string
+	 */
+	public function getContent($content);
+
+    /**
+     * Sets the object passed in to replace items in the content.
+     * @param stdClass $context Ibject passed in to replace items in the content.
+     * @return void
+     */
+    public function setContext(stdClass $context);
+
+    /**
+     * Gets the object passed in to replace items in the content.
+     *
+     * @return stdClass
+     */
+    public function getContext();
+	
+    /**
+     * Renders content given the context object.
+     * @return string content rendered using the values in $context
+     */
+    public function render();
+}
---
+++ src/Text/Interfaces/TemplatedSerializableInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace com\xcitestudios\Generic\Text\Interfaces;
+
+use com\xcitestudios\Generic\Data\Manipulation\Interfaces\SerializationInterface;
+
+/**
+ * General interface for something that can render a template style string and supports serialization.
+ */
+interface TemplatedSerializableInterface 
+	extends TemplatedInterface, SerializationInterface
+{
+}
